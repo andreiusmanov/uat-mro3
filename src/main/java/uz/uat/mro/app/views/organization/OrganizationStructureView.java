3087e24d0bca54e2375ecfe1f1af1f8a97bead00
usmanov1970 <andrei.usmanov@outlook.com>
2023-11-20 18:02:58 +0500

    c1

--- src/main/java/uz/uat/mro/app/views/organization/OrganizationStructureView.java
+++ src/main/java/uz/uat/mro/app/views/organization/OrganizationStructureView.java
@@ -1,5 +1,6 @@
 package uz.uat.mro.app.views.organization;
 
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.tabs.Tab;
@@ -7,18 +8,25 @@ import com.vaadin.flow.component.tabs.TabSheet;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
+import uz.uat.mro.app.model.documents.organization.OrganizationStructure;
+import uz.uat.mro.app.model.documents.organization.OrganizationUnit;
 import uz.uat.mro.app.model.documents.organization.StructureService;
+import uz.uat.mro.app.utils.Keys;
+import uz.uat.mro.app.utils.MyUtils;
 
 @PageTitle(value = "Орг. Структура")
 @Route(value = "org-structure")
 public class OrganizationStructureView extends VerticalLayout {
     private StructureService service;
+    private OrganizationStructure structure;
     private TabSheet tabSheet;
     private Tab listTab;
     private Tab chartTab;
+    private Grid<OrganizationUnit> grid;
 
     public OrganizationStructureView(StructureService service) {
         this.service = service;
+        this.structure = (OrganizationStructure) MyUtils.getAttribute(Keys.STRUCTURE);
         tabs();
         add(tabSheet);
 
@@ -26,10 +34,25 @@ public class OrganizationStructureView extends VerticalLayout {
 
     private void tabs() {
         this.tabSheet = new TabSheet();
-        this.listTab = new Tab("Список");
-        listTab.add(new H3("Орг структура в виде списка"));
-        this.chartTab = new Tab("График");
-        chartTab.add(new H3("Орг структура в виде графика"));
+        listTab = tabSheet.add("Список", listData());
+        listTab.add(listData());
+        chartTab = tabSheet.add("График", chartData());
+    }
+
+    private VerticalLayout listData() {
+        H3 lo = new H3("Орг структура в виде списка");
+        VerticalLayout v = new VerticalLayout();
+        Grid<OrganizationUnit> grid = new Grid<>(OrganizationUnit.class);
+        v.add(lo, grid);
+        return v;
+    }
+
+    private VerticalLayout chartData() {
+        H3 lo = new H3("Орг структура в виде графика");
+        VerticalLayout v = new VerticalLayout();
+        Grid<OrganizationUnit> grid = new Grid<>(OrganizationUnit.class);
+        v.add(lo, grid);
+        return v;
     }
 
 }
