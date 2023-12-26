cc1161aa605dadee07a8fc0139e0f74bac1987fd
usmanov1970 <usmanov.ivoice@outlook.com>
2023-12-26 10:38:34 +0500

    c0

--- src/main/java/uz/uat/mro/app/views/organization/OrganizationsView.java
+++ src/main/java/uz/uat/mro/app/views/organization/OrganizationsView.java
@@ -2,125 +2,84 @@ package uz.uat.mro.app.views.organization;
 
 import java.util.List;
 
+import org.vaadin.crudui.crud.impl.GridCrud;
+
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.contextmenu.MenuItem;
+import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.grid.dataview.GridListDataView;
-import com.vaadin.flow.component.icon.VaadinIcon;
-import com.vaadin.flow.component.menubar.MenuBar;
-import com.vaadin.flow.component.menubar.MenuBarVariant;
-import com.vaadin.flow.component.notification.Notification;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
 import uz.uat.mro.app.model.documents.organization.OrganizationService;
 import uz.uat.mro.app.model.documents.organization.OrganizationUnit;
+import uz.uat.mro.app.model.documents.organization.OrganizationUnitType;
+import uz.uat.mro.app.model.terms.common.Country;
 import uz.uat.mro.app.utils.Keys;
 import uz.uat.mro.app.utils.MyUtils;
 import uz.uat.mro.app.views.MainLayout;
 
 @PageTitle(value = "Список Организаций")
-@Route(value = "organizations", layout = MainLayout.class)
+@Route(value = "data/organizations", layout = MainLayout.class)
 public class OrganizationsView extends VerticalLayout {
     private OrganizationService service;
-    private Grid<OrganizationUnit> grid;
+    private GridCrud<OrganizationUnit> crud;
     private OrganizationUnit organization;
-    private MenuBar menu;
-    private MenuItem viewItem;
-    private MenuItem addItem;
-    private MenuItem deleteItem;
-    private MenuItem facilitiesItem;
-    private MenuItem specialItem;
-    private MenuItem reportItem;
-    private TextField searchField;
-    private Button searchButton;
     private GridListDataView<OrganizationUnit> dataView;
-    private HorizontalLayout operations;
+    private Button viewButton;
 
     public OrganizationsView(OrganizationService service) {
         this.service = service;
         grid();
-        menu();
-        operations();
-        add(operations, grid);
-    }
-
-    private void operations() {
-        this.operations = new HorizontalLayout();
-        this.operations.add(searchField, searchButton, menu);
-    }
-
-    private void menu() {
-        this.menu = new MenuBar();
-        menu.addThemeVariants(MenuBarVariant.LUMO_TERTIARY);
-
-        this.searchField = new TextField();
-        this.searchButton = new Button(VaadinIcon.SEARCH.create());
-        dataView = grid.getListDataView();
-
-        menu.addItem(searchField);
-        menu.addItem(searchButton);
-
-        searchButton.addClickListener(click -> {
-            dataView.removeFilters();
-            dataView.setFilter(unit -> unit.getCode().contains(searchField.getValue())
-                    || unit.getName().contains(searchField.getValue()));
-        });
-
-        searchField.addValueChangeListener(event -> {
-            dataView.removeFilters();
-        });
-
-        viewItem = menu.addItem("Обзор", " Просмотреть данные об организации", click -> {
-            MyUtils.setAttribute(Keys.ORGANIZATION, organization);
-            UI.getCurrent().navigate(OrganizationView.class);
-        });
-
-        facilitiesItem = menu.addItem("Объекты", " Просмотреть данные об объектах организации", click -> {
-            Notification.show("Просмотр объектов");
-        });
-        reportItem = menu.addItem("Отчеты");
-        reportItem.addClickListener(click -> {
-            Notification.show("Отчеты");
-        });
-        specialItem = menu.addItem("Спец. операции", "Спец. операции", click -> {
-            Notification.show("Спец. операции");
-        });
-        addItem = specialItem.getSubMenu().addItem("Добавить");
-        addItem.addClickListener(click -> {
-            MyUtils.setAttribute(Keys.PROJECT, new OrganizationUnit());
-            UI.getCurrent().navigate(OrganizationView.class);
-        });
-        deleteItem = specialItem.getSubMenu().addItem("Удалить");
-        deleteItem.addClickListener(click -> {
-            Notification.show("Удалить");
-        });
-        viewItem.setEnabled(false);
-        // editItem.setEnabled(false);
-        // unitsItem.setEnabled(false);
-        facilitiesItem.setEnabled(false);
-        deleteItem.setEnabled(false);
+        add(crud);
     }
 
     private void grid() {
-        this.grid = new Grid<>(OrganizationUnit.class);
+        this.crud = new GridCrud<>(OrganizationUnit.class);
         List<OrganizationUnit> units = service.findMainOrganizations("Организация");
 
-        this.grid.getSelectionModel().addSelectionListener(selected -> {
+        Grid<OrganizationUnit> grid = crud.getGrid();
+
+        grid.getSelectionModel().addSelectionListener(selected -> {
             boolean res = !grid.getSelectedItems().isEmpty();
-            viewItem.setEnabled(res);
-            facilitiesItem.setEnabled(res);
-            deleteItem.setEnabled(res);
+            this.viewButton.setEnabled(res);
             this.organization = grid.getSelectionModel().getFirstSelectedItem().orElse(new OrganizationUnit());
         });
-        this.grid.setItems(units);
-        this.grid.setColumns("name", "code", "description");
-        this.grid.getColumnByKey("name").setHeader("Наименование");
-        this.grid.getColumnByKey("code").setHeader("Код");
-        this.grid.getColumnByKey("description").setHeader("Описание");
+        grid.setItems(units);
+        grid.setColumns("name", "code", "description");
+        grid.getColumnByKey("name").setHeader("Наименование");
+        grid.getColumnByKey("code").setHeader("Код");
+        grid.getColumnByKey("description").setHeader("Описание");
+
+        crud.setAddOperation(service::save);
+        crud.setUpdateOperation(service::save);
+        crud.setDeleteOperation(service::delete);
+        crud.setFindAllOperation(() -> service.findMainOrganizations("Организация"));
+
+        crud.getCrudFormFactory().setVisibleProperties("name", "code", "description", "shortName", "country", "type");
+        crud.getCrudFormFactory().setFieldCaptions("Наименование", "Код", "Описание", "Аббрев.", "Страна", "Тип");
+        crud.getCrudFormFactory().setFieldProvider("type", type -> {
+            ComboBox<OrganizationUnitType> combo = new ComboBox<>("Тип", service.findAllOrganizationTypes());
+            combo.setItemLabelGenerator((c) -> c.getName());
+            return combo;
+        });
+        crud.getCrudFormFactory().setFieldProvider("country", type -> {
+            ComboBox<Country> combo = new ComboBox<>("Страна", service.findAllCountries());
+            combo.setItemLabelGenerator((c) -> c.getName() + " (" + c.getCode3() + ")");
+            return combo;
+        });
+        button();
+        crud.getContent().add(viewButton);
     }
+
+    private void button() {
+        this.viewButton = new Button("Обзор");
+        viewButton.addClickListener(click -> {
+            MyUtils.setAttribute(Keys.ORGANIZATION, organization);
+            UI.getCurrent().navigate(OrganizationView.class);
+        });
+    }
+
 }
